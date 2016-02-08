548793dd555babeb1d586d38dde89888f4fcda38
Vincent BAUGUEN <vbauguen@e1r10p3.42.fr>
2016-02-08 14:18:03 +0100

    fdf du 08/02/2016

--- Projects/FdF/header.h
+++ Projects/FdF/header.h
@@ -6,22 +6,32 @@
 /*   By: vbauguen <vbauguen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 20:48:17 by vbauguen          #+#    #+#             */
-/*   Updated: 2016/02/01 22:53:03 by vbauguen         ###   ########.fr       */
+/*   Updated: 2016/02/08 10:48:05 by vbauguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
+# define BUFF_SIZE 30
+
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
 typedef struct	s_id
 {
 	void *win;
 	void *mlx;
+	int x0;
 	int x1;
-	int x2;
 	int y1;
-	int y2;
-
+	int y0;
+	int dx;
+	int dy;
+	int err;
+	int sy;
+	int sx;
+	int e2;
 }				t_id;
 
 #endif
--- Projects/FdF/main.c
+++ Projects/FdF/main.c
@@ -15,74 +15,148 @@
 #include "header.h"
 #include "mlx.h"
 #include <stdio.h>
+#include "libft/libft.h"
 
-void ft_Bresenham(t_id *s)
-{	
-	// printf("tab[0][0] , aka XO = %d\n", tab[0][0]);
-	// printf("tab[0][1] , aka Y0 = %d\n", tab[0][1]);
-	// printf("tab[1][0] , aka X1 = %d\n", tab[1][0]);
-	// printf("tab[1][1] , aka Y1 = %d\n", tab[1][1]);
-
-	float dx, dy, p;
-	int i, x, y, xend;
-
-	s->x1 = 500;
-	s->x2 = 250;
-	s->y1 = 500;
-	s->y2 = 100;
-	dx = s->x2 - s->x1;
-	dy = s->y2 - s->y1;
-	p = 2 * dy - dx;
-	if (s->x1 > s->x2)
-	{
-		x = s->x2;
-		y = s->y2;
-		xend = s->x1;
-	}
-	else 
-	{
-		x = s->x1;
-		y = s->y1;
-		xend = s->x2;
-	}
-	mlx_pixel_put(s->mlx, s->win, x, y, 0x00FF0000);
-	while (x < xend)
-	{
-		if (p < 0)
-		{
-			x++;
-			mlx_pixel_put(s->mlx, s->win, x, y, 0x00FF0000);
-			p = p + (2 * dy);
-		}
-		else
-		{
-			x++;
-			y++;
-			mlx_pixel_put(s->mlx, s->win, x, y, 0x00FF0000);
-			p = p + (2 * dy) - (2 * dx);
-		}
-	}
-
-
-}
-int key_reaction(int keycode, t_id *param)
+int ft_abs(int n)
 {
-	int tab[2][2] = {{600, 450}, {600, 200}}; 	//on doit pouvoir recuperer [j][i] au lieu de [2][2] lors de la lecture du fichier
-	int y;											// i etant le nombre de colonnes et j le nomre de lignes
-	int x;
+	if (n < 0)
+		return (-n);
+	return (n);
+}
 
+
+
+void ft_bresenham(int tab[2][2], t_id *s)
+{
+	s->x0 = tab[0][0];
+	s->y0 = tab[0][1];
+	s->x1 = tab[1][0];
+	s->y1 = tab[1][1];
+	s->dx = ft_abs(s->x1 - s->x0);
+	s->dy = ft_abs(s->y1 - s->y0);
+	s->sx = s->x0 < s->x1 ? 1 : -1;
+	s->sy = s->y0 < s->y1 ? 1 : -1;
+	s->err = (s->dx > s->dy ? s->dx : -s->dy) / 2;
+	while (1)
+	{
+ 	   mlx_pixel_put(s->mlx, s->win, s->x0, s->y0, 0x00FF00FF);
+    	if (s->x0 == s->x1 && s->y0 == s->y1)
+    		break;
+   		s->e2 = s->err;
+    	if (s->e2 > -s->dx)
+    	{
+    		s->err-= s->dy;
+    		s->x0+= s->sx;
+    	}
+    	if (s->e2 < s->dy)
+    	{
+    		s->err+= s->dx;
+    		s->y0+= s->sy; 
+    	}
+	}
+}
+
+int key_reaction(int keycode, t_id *param, int **map)
+{
+	int tab[2][2] = {{100, 100}, {1000, 100}}; 	//on doit pouvoir recuperer [j][i] au lieu de [2][2] lors de la lecture du fichier
+	// int y;											// i etant le nombre de colonnes et j le nomre de lignes
+	// int x;
+	map = ft_parsing();
 	if (keycode == 53)
 		exit(0);
 	if (keycode == 49)
-		ft_Bresenham(param);
+		ft_bresenham(tab, param);
 	return (0);	
 }
 
-int main()
+void ft_print_int_2d_tab(int **map , int i)
+{
+	int j;
+	int k;
+
+	j = 0;
+	k = 0;
+	printf("Avant le premier while\n");
+	while (k < i)
+	{
+		j = 0;
+		printf("Avant le deuxieme while\n");
+		while (j < i)
+		{
+			ft_putnbr(map[k][j]);
+			ft_putchar(' ');
+			j++;
+		}
+		printf("Apres le deuxieme while\n");
+		ft_putendl("");
+		k++;
+	}
+
+}
+
+int **ft_parsing(char *str)
+{
+	int *tab;
+	int i;
+	static int j;
+	char **char_to_int;
+	int **map;
+
+	tab = (int *)malloc(sizeof(int) * ft_get_number_of_words(str, ' '));
+	char_to_int = ft_strsplit(str, ' ');
+	j = 0;
+	i = 0;
+
+	map = (int **)malloc(sizeof(int *) * ft_get_number_of_lines(str));
+
+	while (char_to_int[i])
+	{
+		tab[i] = ft_atoi(char_to_int[i]);
+		// printf("%d ", ft_atoi(char_to_int[i]));
+		i++;
+	}
+	map[j] = tab;
+	j++;
+	// k = 0;
+	// l = 0;
+	// while (k < i)
+	// {	
+	// 	while (l < i)
+	// 	{
+	// 		printf("%d ", map[k][l]);
+	// 		l++;
+	// 	}
+	// 	k++;
+	// }
+	// printf("\n");
+
+
+	// ft_print_int_2d_tab(map, i);
+	return (map);
+}
+
+
+int main(int argc, char **argv)
 {
 	t_id *s;
-	int y;
+	int fd;
+	char *line;
+	int i;
+	(void)argc;
 	int x;
+	int y;
+
+	x = 0;
+	y = 0;	
+	fd = open(argv[1], O_RDONLY);
+	i = 0;
+	while ((get_next_line(fd, &line)) > 0) 
+	{	
+		ft_parsing(line);
+		++i;
+	}
+	// printf("Nombre total de tours : %d\n", i);
+
 
 	s = (t_id*)malloc(sizeof(t_id));
 	s->mlx = mlx_init();
@@ -94,7 +168,7 @@ int main()
 		x = 300;
 		while (x < 900)
 		{
-			mlx_pixel_put(s->mlx, s->win, x, y, 0x00000000);
+			mlx_pixel_put(s->mlx, s->win, x, y, 0x00FFFFFF);
 			x++;
 		}
 		y++;	
@@ -104,4 +178,5 @@ int main()
 
 	mlx_key_hook(s->win, key_reaction, s);
 	mlx_loop(s->mlx);
+	return (0);
 }
