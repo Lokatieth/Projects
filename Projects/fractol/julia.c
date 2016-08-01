8845e7f3f268004cccc78b435c799d9dbd3f3846
Vincent <vincent.bauguen@gmail.com>
2016-08-01 14:22:17 +0200

    exercice 00

--- Projects/fractol/julia.c
+++ Projects/fractol/julia.c
@@ -14,6 +14,13 @@
 
 int		ft_julia_mouse(int x, int y, t_id *s)
 {
+	static int i = 0;
+
+	i++;
+
+	if (i % 10 == 0)
+	{
+
 	if (s->mouse_movement == 1)
 	{
 		s->c_r = s->c_r_base * y / W_Y;
@@ -21,6 +28,7 @@ int		ft_julia_mouse(int x, int y, t_id *s)
 		ft_recreate_img(s);
 		ft_draw(s);
 	}
+}
 	return (0);
 }
 
